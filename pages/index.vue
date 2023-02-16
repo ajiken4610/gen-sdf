05bfa0739f09970f17c0ce578e16101190dd4714
ajiken4610 <ajiken4610@gmail.com>
2023-02-16 21:12:58 +0900

    step1のシェーダ準備

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,17 @@
 <template lang="pug">
+
+
+
+
 div
 </template>
 
 <script setup lang="ts">
-import { CanvasTexture, Mesh, MeshBasicMaterial, OrthographicCamera, PlaneGeometry, Scene, WebGLRenderer } from "three";
+
+
+
+
+import { CanvasTexture, Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, WebGLRenderTarget, WebGLRenderer } from "three";
 // init renderer
 const renderer = new WebGLRenderer()
 renderer.setSize(512, 512)
@@ -21,12 +29,11 @@ camera.lookAt(0, 0, 0)
 const scene = new Scene()
 // init mesh
 const planeG = new PlaneGeometry(2, 2)
-const planeM = new MeshBasicMaterial({ color: 0xffffff })
 onUnmounted(() => {
   planeG.dispose()
-  planeM.dispose()
 })
-scene.add(new Mesh(planeG, planeM))
+const plane = new Mesh(planeG)
+scene.add(plane)
 
 // init srcTex
 const hello = await generateTextedCanvas("Hello", { width: 512, height: 512, maxWidth: 512, originalRatio: true })
@@ -34,8 +41,40 @@ const world = await generateTextedCanvas("World", { width: 512, height: 512, max
 // document.body.appendChild(hello.canvas)
 const helloTex = new CanvasTexture(hello.canvas)
 const worldTex = new CanvasTexture(world.canvas)
-planeM.map = helloTex
 
 // render
+renderer.render(scene, camera)
+
+let srcTarget = new WebGLRenderTarget(512, 512)
+let dstTarget = new WebGLRenderTarget(512, 512);
+onUnmounted(() => {
+  srcTarget.dispose()
+  dstTarget.dispose()
+  srcTarget.texture.dispose()
+  dstTarget.texture.dispose()
+})
+
+// step1: 周囲4px
+const step1Shader = new ShaderMaterial({
+  vertexShader:/* glsl */`
+varying vec2 v_UV;
+void main(){
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  v_UV = uv;
+}`,
+  fragmentShader:/* glsl */`
+uniform sampler2D map;
+varying vec2 v_UV;
+void main(){
+  gl_FragColor = texture2D(map,v_UV);
+}`,
+  uniforms: {
+    map: { value: helloTex }
+  }
+})
+onUnmounted(() => {
+  step1Shader.dispose()
+})
+plane.material = step1Shader;
 renderer.render(scene, camera)
 </script>
