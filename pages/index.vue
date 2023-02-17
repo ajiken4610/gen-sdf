d74b385ec707398dff77c87cba69c586c97279f4
ajiken4610 <ajiken4610@gmail.com>
2023-02-17 17:22:11 +0900

    多分完成？

--- pages/index.vue
+++ pages/index.vue
@@ -161,5 +161,151 @@ onUnmounted(() => {
   step1Shader.dispose()
 })
 plane.material = step1Shader;
+renderer.setRenderTarget(srcTarget);
+renderer.render(scene, camera)
+// step2
+const step2Shader = new ShaderMaterial({
+  vertexShader:/* glsl */`
+varying vec2 v_UV;
+void main(){
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  v_UV = uv;
+}`,
+  fragmentShader:/* glsl */`
+uniform sampler2D map;
+uniform float w;
+uniform float h;
+uniform float f;
+varying vec2 v_UV;
+
+vec2 unpackDistance(in vec2 packed){
+  vec2 normalized = (packed - .5) * 2.;
+  return pow(vec2(2.),abs(normalized * 16.)) * sign(normalized);
+}
+
+vec2 normalizeCoord(in vec2 raw){
+  vec2 logged = log2(abs(raw) + 1.) * sign(raw) / 16.;
+  return (logged + 1.) / 2.;
+}
+
+vec2 addOffset(in float x,in float y){
+  return vec2(v_UV.x + x * w * f,v_UV.y + y * h * f);
+}
+
+bool aIsMinThanB(in vec2 aCoord,in vec4 a,in vec2 bCoord,in vec4 b){
+  if(a.z == 0.){
+    return false;
+  }
+  if(b.z == 0.){
+    return true;
+  }
+  vec2 aDist = unpackDistance(a.xy);
+  vec2 bDist = unpackDistance(b.xy);
+  float aLen = length(aDist + aCoord);
+  float bLen = length(bDist + bCoord);
+  if(aLen < bLen){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+void updateDatas(inout vec2 minCoord,inout vec4 minData,in float offsetX,in float offsetY){
+  vec2 offset = addOffset(offsetX,offsetY);
+  vec4 data = texture2D(map,offset);
+  if(!aIsMinThanB(minCoord,minData,offset,data)){
+    minCoord = vec2(offsetX,offsetY);
+    minData = data;
+  }
+}
+
+void main(){
+  vec2 minCoord = vec2(0.,0.);
+  vec4 center = texture2D(map,v_UV);
+  vec4 minData = center;
+
+  updateDatas(minCoord,minData,1.,0.);
+  updateDatas(minCoord,minData,-1.,0.);
+  updateDatas(minCoord,minData,2.,0.);
+  updateDatas(minCoord,minData,-2.,0.);
+  updateDatas(minCoord,minData,3.,0.);
+  updateDatas(minCoord,minData,-3.,0.);
+  updateDatas(minCoord,minData,0.,1.);
+  updateDatas(minCoord,minData,0.,-1.);
+  updateDatas(minCoord,minData,0.,2.);
+  updateDatas(minCoord,minData,0.,-2.);
+  updateDatas(minCoord,minData,0.,3.);
+  updateDatas(minCoord,minData,0.,-3.);
+
+  gl_FragColor = vec4(normalizeCoord(unpackDistance(minData.xy) + minCoord * f),minData.z,1.);
+  // gl_FragColor = vec4((unpackDistance(center.xy) + minCoord * f) / 4.,0.,1.);
+}
+`,
+  uniforms: {
+    map: { value: null },
+    w: { value: 1 / 512 },
+    h: { value: 1 / 512 },
+    f: { value: null }
+  }
+})
+onUnmounted(() => {
+  step2Shader.dispose()
+})
+plane.material = step2Shader;
+for (var i = 0; i < 3; i++) {
+  step2Shader.uniforms["f"]!.value = 4 ** (i + 1);
+  step2Shader.uniforms["map"]!.value = srcTarget.texture;
+  renderer.setRenderTarget(dstTarget);
+  renderer.render(scene, camera);
+  [srcTarget, dstTarget] = [dstTarget, srcTarget];
+}
+// renderer.setRenderTarget(null)
+// renderer.render(scene, camera)
+// step3
+const step3Shader = new ShaderMaterial({
+  vertexShader:/* glsl */`
+varying vec2 v_UV;
+void main(){
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  v_UV = uv;
+}`,
+  fragmentShader:/* glsl */`
+uniform sampler2D map;
+uniform sampler2D tex;
+uniform float w;
+uniform float h;
+varying vec2 v_UV;
+vec2 unpackDistance(in vec2 packed){
+  vec2 normalized = (packed - .5) * 2.;
+  return pow(vec2(2.),abs(normalized * 16.)) * sign(normalized);
+}
+float normalizeLength(in float raw){
+  float logged = log2(abs(raw) + 1.) * sign(raw) / 16.;
+  return (logged + 1.) / 2.;
+}
+void main(){
+  vec4 data = texture2D(map,v_UV);
+  vec2 unpacked = unpackDistance(data.xy);
+  float length = length(unpacked);
+  length = texture2D(tex,v_UV).r > .5 ? length : -length;
+  length = normalizeLength(length);
+  gl_FragColor = vec4(vec3(length),1.);
+}
+`,
+  uniforms: {
+    map: { value: null },
+    tex: { value: null },
+    w: { value: 1 / 512 },
+    h: { value: 1 / 512 },
+    f: { value: null }
+  }
+})
+onUnmounted(() => {
+  step3Shader.dispose()
+})
+step3Shader.uniforms["map"]!.value = srcTarget.texture;
+step3Shader.uniforms["tex"]!.value = helloTex;
+plane.material = step3Shader
+renderer.setRenderTarget(null)
 renderer.render(scene, camera)
 </script>
